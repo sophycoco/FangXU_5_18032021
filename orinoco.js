dad656b336cad91ed02c78d989ef309b03dcd4c2
xf <sophycoco@gmail.com>
2021-07-05 10:34:43 +0200

    Update

--- orinoco.js
+++ orinoco.js
@@ -1,9 +1,43 @@
 fetch("http://localhost:3000/api/cameras")
-    .then( date => date.json())
-    .then( jsonCameraList=> {
-        console.log( jsonCameraList);
-        for( let jsonCamera = jsonCameraList) {
-            let camera = new Camera(jsonCamera);
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (value) {
+    console.log(value);
+    document.querySelector(".container").innerHTML += `<div class="">
+                                                            <div class="card article">
+                                                                <div class="card-header ">
+                                                                    <h3 class="card-title">(${value.name})</h3>
+                                                                </div>
+                                                                <img src="${image}" class="card-img-top">
+                                                                <div class="card-body">
+                                                                    <p class="card-text">${value.description}</p>
+                                                                    <!--lenses-->
+                                                                    <p class="card-price">${value.price}</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>`;
+  })
+  .catch(function (err) {
+    //An error has occurred.
+  });
 
-        }
-    })
+/*function addArticle() {
+  document.querySelector(".container").innerHTML += `<div class="">
+                                                            <div class="card article">
+                                                                <div class="card-header ">
+                                                                    <h3 class="card-title">${article.name}</h3>
+                                                                </div>
+                                                                <img src="${image}" class="card-img-top">
+                                                                <div class="card-body">
+                                                                    <p class="card-text">${article.description}</p>
+                                                                    <!--lenses-->
+                                                                    <p class="card-price">${article.price}</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>`;
+}
+
+addArticle(); */
