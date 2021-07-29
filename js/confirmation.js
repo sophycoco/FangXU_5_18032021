769d41b9df5261cee91390683eba717a8a7913e5
xf <sophycoco@gmail.com>
2021-07-29 13:40:26 +0200

    Update confirmation page - no content when cart is empty

--- js/confirmation.js
+++ js/confirmation.js
@@ -4,6 +4,8 @@ const totalPrice = localStorage.getItem("totalPrice");
 
 // add to HTML
 const confirmId = document.querySelector("#sumOrder");
+if (totalPrice != 0 && totalPrice != null) {
 confirmId.innerHTML += 
-`<p class="center">Votre commande N° <span class="insert">${orderId} </span>à bien été prises en compte.</p>
-<p class="center">Le montant de votre commande est de : ${totalPrice} <span class="insert"></span>euros. </p>`;
+`<h2>Merci pour votre commande.</h2>
+<p class="center">Votre commande N° <span class="insert">${orderId} </span>à bien été prises en compte.</p>
+<p class="center">Le montant de votre commande est de : ${totalPrice} <span class="insert"></span>euros. </p>`;}
